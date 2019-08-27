d4c61b34271cf8e682fbf2466150d18830505c22
danilo <danilo.germano@etec.sp.gov.br>
2019-08-26 22:13:45 -0300

    inicial

---
+++ app/src/main/java/br/com/etecia/testerecyclerview/Modelo.java
@@ -0,0 +1,13 @@
+package br.com.etecia.testerecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Modelo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modelo_layout);
+    }
+}
